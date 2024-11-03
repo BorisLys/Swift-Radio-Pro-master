b1d1e8b394ba5398f19078ffd9d60f40e5a046ca
Boris lysikov <festival311@gmail.com>
2024-11-03 23:49:44 +0700

    add

---
+++ SwiftRadioTests/SwiftRadioTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftRadioTests.swift
+//  SwiftRadioTests
+//
+//  Created by Lysikov Boris on 03.11.2024.
+//  Copyright © 2024 matthewfecher.com. All rights reserved.
+//
+
+import Testing
+
+struct SwiftRadioTests {
+
+    @Test func example() async throws {
+        #expect(2 > 1)
+    }
+}
