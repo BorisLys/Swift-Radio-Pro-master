e8df8550f46c0f26352ed7f457174991058f6a6a
Boris lysikov <festival311@gmail.com>
2024-11-18 22:58:13 +0700

    Add TestObserver

---
+++ SwiftRadioUITests/Tests/TestObserver.swift
@@ -0,0 +1,9 @@
+//
+//  TestObserver.swift
+//  SwiftRadioUITests
+//
+//  Created by Lysikov Boris on 17.11.2024.
+//  Copyright © 2024 matthewfecher.com. All rights reserved.
+//
+
+import Foundation
