7cab151ed73107b43935c49460ec41b5cbbe8dd9
Boris Lysikov <b.lysikov@dodobrands.io>
2025-01-22 21:13:14 +0700

    add marathon

--- SwiftRadioUITests/Page/StationInfoPage.swift
+++ SwiftRadioUITests/Page/StationInfoPage.swift
@@ -20,8 +20,4 @@ struct StationInfoPage: BaseScreen, NavigationBar {
     static let okayButton = app.buttons[id.okayButton]
     static let stationDeskLabel = app.staticTexts[id.stationDescLabel]
     static let stationTextView = app.textViews[id.stationLongDescTextView]
-        
-    private let stationDescDescription = "The Music Starts Here"
-    private let textViewDescription = "All your favorite country hits and artists, from Johnny Cash to Taylor Swift, on 1.FM's Absolute Country, playing non-stop crooners and banjos, dance-tunes and fiddles, ballads and harmonicas. Absolute Country focuses on 5th, 6th and 7th generation Country (from the 90s on) but often delves into classic, older tunes as well."
-    
 }
